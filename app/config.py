ead5e2fec041475f392bea5e80c12472151f4093
Michael Peels <michaelpeels@Michael-Peels.local>
2024-06-27 16:02:38 -0400

    Set defaults for db connection

--- app/config.py
+++ app/config.py
@@ -8,20 +8,25 @@ from pydantic import Field
 class Settings(BaseSettings):
     mpi_db_type: str = Field(
         description="The type of database used by the MPI",
+        default="postgres"
     )
     mpi_dbname: str = Field(
         description="The name of the database used by the MPI",
+        default="testdb"
     )
     mpi_host: str = Field(
         description="The host name of the MPI database",
+        default="localhost"
     )
     mpi_user: str = Field(
         description="The name of the user used to connect to the MPI database",
+        default="postgres"
     )
     mpi_password: str = Field(
         description="The password used to connect to the MPI database",
+        default="postgres"
     )
-    mpi_port: str = Field(description="The port used to connect to the MPI database")
+    mpi_port: str = Field(description="The port used to connect to the MPI database", default="5432")
     connection_pool_size: Optional[int] = Field(
         description="The number of MPI database connections in the connection pool",
         default=5,
@@ -43,4 +48,4 @@ def get_settings() -> dict:
     :return: A dictionary with keys specified by the Settings. The value of each key is
     read from the corresponding environment variable.
     """
-    return Settings().dict()
+    return Settings().model_dump()
